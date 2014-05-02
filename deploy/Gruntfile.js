ba0862c1d4a197fe37cc5e8f153f9ac051c20fd0
Mason Braun <masonbraun@gmail.com>
2014-05-02 15:06:05 -0400

    sass setup with grunt watch and working

--- deploy/Gruntfile.js
+++ deploy/Gruntfile.js
@@ -1,20 +1,19 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
 
-        compass: {                  // Task
-            dist: {                   // Target
-              options: {              // Target options
-                sassDir: 'css/sass',
-                cssDir: 'css',
-                outputStyle: 'compressed',
-                cacheDir: 'css/sass/.sass-cache/'
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'css/sass',
+                    cssDir: 'css',
+                    outputStyle: 'compressed',
+                    cacheDir: 'css/sass/.sass-cache/'
                 }
             }
-          },
+        },
 
         watch: {
             css: {
@@ -24,12 +23,9 @@ module.exports = function(grunt) {
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-    // Default task(s).
     grunt.registerTask('default', ['watch']);
 
 };
