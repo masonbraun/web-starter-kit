fec48b46501403c9730425f21756bf3e5fab57fb
Mason Braun <masonbraun@gmail.com>
2014-05-05 15:09:28 -0400

    setup sass lang detection

--- deploy/Gruntfile.js
+++ deploy/Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
                     sassDir: 'css/sass',
                     cssDir: 'css',
                     imagesDir: 'img',
-                    outputStyle: 'compressed',
+                    outputStyle: 'nested',
                     cacheDir: 'css/sass/.sass-cache/'
                 }
             }
