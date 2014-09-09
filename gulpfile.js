b00538009ce3233e9a661642101c420b908b5735
Mason Braun <masonbraun@gmail.com>
2014-09-08 23:07:45 -0400

    switch to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,6 @@ var clean    = require('gulp-clean');
 var ignore   = require('gulp-ignore');
 var cache    = require('gulp-cache');
 
-
 gulp.task('default', function() {
   // place code for your default task here
 });
@@ -31,38 +30,6 @@ gulp.task('styles', function() {
         }))
         .pipe(gulp.dest(config.dirs.src + 'css'))
         .pipe(notify({ message: 'Styles task complete' }));
-});
-
-// delete the dist directory
-gulp.task('clean', function() {
-    return gulp.src(['./dist'], {read: false})
-    .pipe(clean());
-});
-
-//copy wordpress into dist directory exluding the wp-content folder
-gulp.task('copy-wp', ['clean'], function() {
-    return gulp.src(["./src/wp/**", "!./src/wp/{wp-content,wp-content/**}"])
-    .pipe(gulp.dest("./dist"));
-});
-
-//copy wp-content directory
-gulp.task('copy-content', ['copy-wp'], function() {
-    return gulp.src(["./src/wp-content/**"])
-    .pipe(gulp.dest("./dist/wp-content"));
-});
-
-//copy wp-config file
-gulp.task('copy-config', ['copy-content'], function() {
-    return gulp.src(["./src/wp-config.php"])
-    .pipe(gulp.dest("./dist"));
-});
-
-//minify images and replace in dist folder
-gulp.task('images', function() {
-    return gulp.src([config.dirs.src + 'img/**'])
-        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-        .pipe(gulp.dest('./dist/wp-content/themes/masonbraun/img'))
-        .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('watch', function() {
