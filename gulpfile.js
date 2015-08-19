71ab4a81e4047c2692baf222e324eb7c76185771
Mason Braun <masonbraun@gmail.com>
2015-08-19 17:05:17 -0400

    added source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -16,7 +18,9 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
 });
